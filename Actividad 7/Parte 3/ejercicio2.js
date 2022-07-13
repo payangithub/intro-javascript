d008840afcb470460df5aa651ad9094380eca410
Guadalupe Payan Camacho <35412119+payangithub@users.noreply.github.com>
2022-07-13 15:54:00 -0600

    Se actualiza ejercicio2.js de la Actividad 7: Parte 3 <<Closures & recursividad>>, para quitar codigo comentado

--- Actividad 7/Parte 3/ejercicio2.js
+++ Actividad 7/Parte 3/ejercicio2.js
@@ -2,28 +2,6 @@
     Ejercicio 2:
     Crea una funcion para calcular la sucesion Fibonacci.
 */
-/* function Fibonacci(numero) {
-    var aux = [];
-    aux[0] = 0;
-    aux[1] = 1;
-    for (var i = 2; i < numero; i++) {
-        aux[i] = aux[i - 2] + aux[i - 1];
-    }
-    //Falta poner la recursividad OJO.
-
-    return aux;
-}
-//var f = Fibonacci(10);
-var secuenciaFibonacci=prompt("Favor de proporcionar el numero para la secuencia Fibonacci:")
-console.log(Fibonacci(secuenciaFibonacci)); */
-
-//Funcion recursiva Fibonacci.
-const fibonacci = (numero) =>{
-    if(numero<2) return numero;
-
-    return fibonacci(numero-1)+fibonacci(numero-2);
-}
-console.log(fibonacci(2));
 
 //Ejemplo seccuencia Fibonacci [0, 1, 1, 2, 3, 5, 8, 13, 21,...]
 //Se crea la funcion de forma Arrow Function.
